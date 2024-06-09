5bb1f2688e56ccaff7b0aec70b1b51bd78290959
NikhilKumar Jain <jainnikhilkumar1008@gmail.com>
2024-06-09 16:12:48 +0530

    1.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,117 @@
-import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
-import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+// import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
+// import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+// import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+// import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
+// import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+// import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+// import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+// import 'package:ar_flutter_plugin/models/ar_node.dart';
+// import 'package:flutter/material.dart';
+// import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+// import 'package:vector_math/vector_math_64.dart' as vector_math;
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'AR Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   ARSessionManager? arSessionManager;
+//   ARObjectManager? arObjectManager;
+//   ARAnchorManager? arAnchorManager;
+//   ARLocationManager? arLocationManager;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('AR Flutter Demo'),
+//       ),
+//       body: Stack(
+//         children: [
+//           ARView(
+//             onARViewCreated: onARViewCreated,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Future<void> onARViewCreated(
+//       ARSessionManager arSessionManager,
+//       ARObjectManager arObjectManager,
+//       ARAnchorManager arAnchorManager,
+//       ARLocationManager arLocationManager) async {
+//     this.arSessionManager = arSessionManager;
+//     this.arObjectManager = arObjectManager;
+//     this.arAnchorManager = arAnchorManager;
+//     this.arLocationManager = arLocationManager;
+//
+//     arSessionManager.onInitialize(
+//       showFeaturePoints: false,
+//       showPlanes: true,
+//       customPlaneTexturePath: "Images/triangle.png",
+//       showWorldOrigin: true,
+//       handleTaps: true,
+//     );
+//
+//     this.arObjectManager?.onInitialize();
+//
+//     // Set the tap handler
+//     arSessionManager.onPlaneOrPointTap = onPlaneTap;
+//   }
+//
+//   void onPlaneTap(List<ARHitTestResult> hits) {
+//     if (hits.isNotEmpty) {
+//       var hit = hits.firstWhere((hit) => hit.type == ARHitTestResultType.plane);
+//       if (hit != null) {
+//         // Add an object at the tapped location
+//         addObject(hit.worldTransform);
+//       }
+//     }
+//   }
+//
+//   void addObject(vector_math.Matrix4 transform) {
+//     var newNode = ARNode(
+//       type: NodeType.webGLB,
+//       uri: "https://example.com/model.glb", // Replace with your actual model URL
+//       scale: vector_math.Vector3(0.1, 0.1, 0.1),
+//       transformation: transform,
+//     );
+//     arObjectManager?.addNode(newNode);
+//   }
+//
+//   @override
+//   void dispose() {
+//     arSessionManager?.dispose();
+//     super.dispose();
+//   }
+// }
 import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
-import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:vector_math/vector_math_64.dart' as vector_math;
 
 void main() => runApp(MyApp());
@@ -31,10 +135,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  ARSessionManager? arSessionManager;
-  ARObjectManager? arObjectManager;
-  ARAnchorManager? arAnchorManager;
-  ARLocationManager? arLocationManager;
+  late ARSessionManager arSessionManager;
+  late ARObjectManager arObjectManager;
+  late ARAnchorManager arAnchorManager;
+  late ARLocationManager arLocationManager;
 
   @override
   Widget build(BuildContext context) {
@@ -42,25 +146,18 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('AR Flutter Demo'),
       ),
-      body: Stack(
-        children: [
-          ARView(
-            onARViewCreated: onARViewCreated,
-          ),
-        ],
+      body: ARView(
+        onARViewCreated: onARViewCreated,
       ),
     );
   }
 
-  Future<void> onARViewCreated(
-      ARSessionManager arSessionManager,
-      ARObjectManager arObjectManager,
-      ARAnchorManager arAnchorManager,
-      ARLocationManager arLocationManager) async {
-    this.arSessionManager = arSessionManager;
-    this.arObjectManager = arObjectManager;
-    this.arAnchorManager = arAnchorManager;
-    this.arLocationManager = arLocationManager;
+  void onARViewCreated(ARSessionManager sessionManager, ARObjectManager objectManager,
+      ARAnchorManager anchorManager, ARLocationManager locationManager) {
+    arSessionManager = sessionManager;
+    arObjectManager = objectManager;
+    arAnchorManager = anchorManager;
+    arLocationManager = locationManager;
 
     arSessionManager.onInitialize(
       showFeaturePoints: false,
@@ -70,9 +167,8 @@ class _MyHomePageState extends State<MyHomePage> {
       handleTaps: true,
     );
 
-    this.arObjectManager?.onInitialize();
+    arObjectManager.onInitialize();
 
-    // Set the tap handler
     arSessionManager.onPlaneOrPointTap = onPlaneTap;
   }
 
@@ -80,7 +176,6 @@ class _MyHomePageState extends State<MyHomePage> {
     if (hits.isNotEmpty) {
       var hit = hits.firstWhere((hit) => hit.type == ARHitTestResultType.plane);
       if (hit != null) {
-        // Add an object at the tapped location
         addObject(hit.worldTransform);
       }
     }
@@ -93,12 +188,6 @@ class _MyHomePageState extends State<MyHomePage> {
       scale: vector_math.Vector3(0.1, 0.1, 0.1),
       transformation: transform,
     );
-    arObjectManager?.addNode(newNode);
-  }
-
-  @override
-  void dispose() {
-    arSessionManager?.dispose();
-    super.dispose();
+    arObjectManager.addNode(newNode);
   }
 }
